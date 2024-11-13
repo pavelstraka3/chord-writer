487302d05c5f5aa9980405095e7f7cc5e2dec085
pavel.straka <pavel.straka@rcs-kladno.net>
2024-11-13 08:29:31 +0100

    Dependancy update

--- app/songs/[id]/page.tsx
+++ app/songs/[id]/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import SongEditor from "@/app/components/SongEditor";
 
-async function Page({ params }: { params: { id: string } }) {
+async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   try {
     const response = await fetch("http://localhost:3000/songs/" + params.id);
     const data = await response.json();
